29a1b15a5fc9d9e423741c6f34c846c0652cb850
Niklas Arndt <git@niklasarndt.com>
2025-08-26 07:27:14 +0200

    fix: Use correct class name

--- src/app/TitleWithTooltip.tsx
+++ src/app/TitleWithTooltip.tsx
@@ -33,7 +33,7 @@ export function TitleWithTooltip({ title, tooltip }: TitleWithTooltipProps) {
           onClick={() => window.location.reload()}
         >
           {/* Tabler Icons refresh icon (counter-clockwise circular arrow) - https://tabler.io/icons */}
-          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
         </button>
       </div>
       {showTooltip && tooltip && (
